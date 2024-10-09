f5a224ffd616f641a607a9b5ee264b009ab684bd
Neftali Fuentes <108016750+neftaliFuentesCorp@users.noreply.github.com>
2024-10-09 00:58:31 -0300

    Cambios menu general

---
+++ GestionFlotas.business/TbModuloAccionBL.cs
@@ -0,0 +1,51 @@
+﻿using GestionFlotas.dataaccess;
+using GestionFlotas.model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionFlotas.business
+{
+    public class TbModuloAccionBL
+    {
+        private readonly FlotasContext _db;
+        public TbModuloAccionBL(FlotasContext db)
+        {
+            _db = db;
+        }
+        public async Task<TbModuloAccionModel> Obtener(int _TbModuloAccionId)
+        {
+            var ModuloAccion = await (from M in _db.TbModuloAccion
+                                      where M.TbModuloAccionId == _TbModuloAccionId
+                                      select (new TbModuloAccionModel
+                                      {
+                                          TbModuloAccionId = M.TbModuloAccionId,
+                                          TbModuloId = M.TbModuloId,
+                                          Nombre = M.Nombre,
+                                          Icono = M.Icono,
+                                          Url = M.Url,
+                                          EsVisibleMenu = M.EsVisibleMenu,
+                                          Activo = M.Activo,
+                                      })).FirstOrDefaultAsync();
+
+
+            return ModuloAccion;
+        }
+        public async Task<List<TbModuloAccionModel>> ObtenerListByTbModuloId(int _TbModuloId)
+        {
+            List<TbModuloAccionModel> ModulosAccion = await (from M in _db.TbModuloAccion
+                                                             where M.TbModuloId == _TbModuloId && M.EsVisibleMenu == true && M.Activo == true
+                                                             select (new TbModuloAccionModel
+                                                             {
+                                                                 TbModuloAccionId = M.TbModuloAccionId,
+                                                                 TbModuloId = M.TbModuloId,
+                                                                 Nombre = M.Nombre,
+                                                                 Icono = M.Icono,
+                                                                 Url = M.Url,
+                                                                 EsVisibleMenu = M.EsVisibleMenu,
+                                                                 Activo = M.Activo,
+                                                             })).ToListAsync();
+
+
+            return ModulosAccion;
+        }
+    }
+}
---
+++ GestionFlotas.model/TbModuloAccionModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionFlotas.model
+{
+    public class TbModuloAccionModel
+    {
+        public short TbModuloAccionId { get; set; }
+
+        public short TbModuloId { get; set; }
+
+        public string Nombre { get; set; } = null!;
+        public string? Icono { get; set; }
+        public string? Url { get; set; }
+
+        public bool EsVisibleMenu { get; set; }
+
+        public bool Activo { get; set; }
+    }
+}
